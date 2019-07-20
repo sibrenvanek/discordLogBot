7d04602f38009d3656e60f9218a4c7f631083222
MeiJuG <meijug@gmail.com>
2019-07-20 10:31:51 +0200

    add help command

--- src/modules.ts
+++ src/modules.ts
@@ -5,33 +5,35 @@ const commands: Array<Command> = [
 	{
 		description: "Displays the current CPU usage in percentages.",
 		command: "cpu",
-		f: _params => executeCommand("ps -C java -o %cpu")
+		f: async _params => executeCommand("ps -C java -o %cpu")
 	},
 	{
 		description: "Displays the current memory usage in percentages.",
 		command: "memory",
-		f: _params => executeCommand("ps -C java -o %mem")
+		f: async _params => executeCommand("ps -C java -o %mem")
 	},
 	{
-		description: "Reboot the machine.",
+		description: "Reboots the machine.",
 		command: "reboot",
-		f: async _params => {
-			if (_params[0] === "Confirm") {
-				return executeCommand("sudo reboot")
-			}
-			else {
-				return "Are you sure you want to do that? If so please type 'Confirm' after the command"
-			}
-		}
+		f: async params => params[0] === "confirm"
+			? executeCommand("sudo reboot")
+			: "Are you sure you want to do that? If so please type 'confirm' after the command"
 	}
 ]
 
-export const messageHandler: MessageHandler = async message => {
-	const [inputCommand, ...params] = message.content.slice(1).split(" ")
-	const command = commands.find(c => c.command === inputCommand)
+const listCommand = (command: Command): string => ` - ${command.command}: ${command.description}`
 
-	await message.channel.send(command
-		? await command.f(params)
-		: `I can't do everything you know... wtf does ${inputCommand} even mean?!`
-	)
+export const messageHandler: MessageHandler = async message => {
+	const [inputCommand, ...params] = message.content.toLowerCase().slice(1).split(" ")
+
+	if (inputCommand === "help") {
+		await message.channel.send(commands.map(listCommand).join("\n"))
+	} else {
+		const command = commands.find(c => c.command === inputCommand)
+	
+		await message.channel.send(command
+			? await command.f(params)
+			: `I can't do everything you know... wtf does ${inputCommand} even mean?!`
+		)
+	}
 }
