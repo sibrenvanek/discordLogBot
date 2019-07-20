a93063659669af41aee0090bdd7a4a2fee5203fa
MeiJuG <meijug@gmail.com>
2019-07-20 10:51:55 +0200

    added parameter to help function

--- src/modules.ts
+++ src/modules.ts
@@ -27,13 +27,21 @@ export const messageHandler: MessageHandler = async message => {
 	const [inputCommand, ...params] = message.content.toLowerCase().slice(1).split(" ")
 
 	if (inputCommand === "help") {
-		await message.channel.send(commands.map(listCommand).join("\n"))
+		// little bit of spaghetti. This should be a sepperate function
+		if (params.length && params[0]) {
+			const helpCommand = commands.find(c => c.command === params[0])
+			await message.channel.send(helpCommand
+				? listCommand(helpCommand)
+				: `Cannot find '${params[0]}'`)
+		} else {
+			await message.channel.send(commands.map(listCommand).join("\n"))
+		}
 	} else {
 		const command = commands.find(c => c.command === inputCommand)
 	
 		await message.channel.send(command
 			? await command.f(params)
-			: `I can't do everything you know... wtf does ${inputCommand} even mean?!`
+			: `I can't do everything you know... wtf does '${inputCommand}' even mean?!`
 		)
 	}
 }
